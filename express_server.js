6b08668940c1ffb283b72c2c8355283b66007902
Tony Tran <tonytran1645@gmail.com>
2023-03-03 20:26:49 -0500

    Added generateRandomString function

--- express_server.js
+++ express_server.js
@@ -9,6 +9,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -30,7 +32,30 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+const generateRandomString = () => {
+  const alphabet = 'abcdefghigklmnopqrstuvwxyz';
+  const numbers = '1234567890';
+  const alphaNumbric = alphabet + numbers;
+
+  let index = Math.round(Math.random() * 100);
+  if (index > 35) {
+    while (index > 61) {
+      index = Math.round(Math.random() * 100);
+    }
+  }
+  return alphaNumbric[index];
+};
