8015c5ccf6788d4ab6f425a40b00daaa7fb18303
Tony Tran <tonytran1645@gmail.com>
2023-04-19 22:53:06 -0400

    Testing

--- express_server.js
+++ express_server.js
@@ -9,7 +9,7 @@ const hashedPassword = bcrypt.hashSync(password, 10);
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookie());
-
+//console.log("testing")
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
